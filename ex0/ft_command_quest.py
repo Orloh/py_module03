fe031a3901febe1af17ae0d9dbb98b9ae5aa7088
orhernan <ohercelli@gmail.com>
2026-03-09 18:55:17 +0100

    ex0: add ft_command_quest.py to handle sys.argv and input validation

---
+++ ex0/ft_command_quest.py
@@ -0,0 +1,36 @@
+# *********************************************************************** #
+#                                                                         #
+#                                                    :::      ::::::::    #
+#    ft_command_quest.py                           :+:      :+:    :+:    #
+#                                                +:+ +:+         +:+      #
+#    By: orhernan <ohercelli@gmail.com>        +#+  +:+       +#+         #
+#                                            +#+#+#+#+#+   +#+            #
+#    Created: 2026/03/09 18:28:46 by orhernan     #+#    #+#              #
+#    Updated: 2026/03/09 18:31:52 by orhernan    ###   ########.fr        #
+#                                                                         #
+# *********************************************************************** #
+
+import sys
+
+
+def ft_command_quest() -> None:
+    """Interpret and display command-line arguments"""
+    print("=== Command Quest ===")
+
+    args = sys.argv
+    total_args = len(sys.argv)
+
+    if total_args == 1:
+        print("No arguments provided!")
+        print(f"Program name: {args[0]}")
+        print(f"Total arguments: {total_args}")
+    else:
+        print(f"Program name: {args[0]}")
+        print(f"Arguments received: {total_args - 1}")
+        for arg, i in enumerate(args[1:], start=1):
+            print(f"Argument {i}: {arg}")
+        print(f"Total arguments: {total_args}")
+
+
+if __name__ == "__main__":
+    ft_command_quest()
