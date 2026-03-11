f6e3639205bc9f80c139b945ac7d2b0ef04f8736
orhernan <ohercelli@gmail.com>
2026-03-11 18:35:19 +0100

    feat(ex1): complete Score Cruncher with custom exception and list analytics

--- ex1/ft_score_analytics.py
+++ ex1/ft_score_analytics.py
@@ -13,9 +13,18 @@
 import sys
 
 
+class EmptyScoreboardError(Exception):
+    """Excpetion raised when the scoreboard has no valid scores."""
+
+    pass
+
+
 class Scoreboard:
     def __init__(self, scores_str: list[str]) -> None:
         self.__scores = self.__parse_scores(scores_str)
+
+        if len(self.__scores) == 0:
+            raise EmptyScoreboardError("No valid scores to process.")
 
     @staticmethod
     def __parse_scores(scores_str: list[str]) -> list[int]:
@@ -23,10 +32,22 @@ class Scoreboard:
         for score in scores_str:
             try:
                 scores.append(int(score))
-            except ValueError as e:
-                print(f"Parsing Error: {e}")
-                print("Warning: Skipping invalid scores...")
+            except ValueError:
+                print(f"Parsing Error: '{score}' is not a valid input.")
+                print("Warning: Skipping invalid input...")
         return scores
+
+    def get_scores(self) -> list[int]:
+        return self.__scores
+
+    def get_num_players(self) -> int:
+        return len(self.__scores)
+
+    def get_sum_scores(self) -> int:
+        return sum(self.__scores)
+
+    def get_avg_score(self) -> float:
+        return self.get_sum_scores() / self.get_num_players()
 
     def get_high_score(self) -> int:
         return max(self.__scores)
@@ -34,15 +55,28 @@ class Scoreboard:
     def get_low_score(self) -> int:
         return min(self.__scores)
 
+    def get_range_score(self) -> int:
+        return self.get_high_score() - self.get_low_score()
+
 
 def main() -> None:
     print("=== Player Score Analytics ===")
     if len(sys.argv) < 2:
-        print("No scores provided. Usage python3 ft_score_analytcs.py <score1> <score2> ...")
+        msg1 = "No scores provided. "
+        msg2 = "Usage python3 ft_score_analytics.py <score1> <score2> ..."
+        print(msg1 + msg2)
         return
-    board = Scoreboard(sys.argv[1:])
-    print(board.get_high_score())
-    print(board.get_low_score())
+    try:
+        board = Scoreboard(sys.argv[1:])
+        print(f"Scores processed: {board.get_scores()}")
+        print(f"Total players: {board.get_num_players()}")
+        print(f"Total score: {board.get_sum_scores()}")
+        print(f"Average score: {board.get_avg_score():.1f}")
+        print(f"High score: {board.get_high_score()}")
+        print(f"Low score: {board.get_low_score()}")
+        print(f"Score range: {board.get_range_score()}")
+    except EmptyScoreboardError as e:
+        print(e)
 
 
 if __name__ == "__main__":
